46c2b6a2639f043e612e5f782dbd61268a27c96b
lukesleeman <luke.sleeman@gmail.com>
2015-03-24 15:00:12 +1100

    Use actual location

--- mobile/src/main/java/au/com/lukesleeman/trainwatch/TrainWatchMessageListenerService.java
+++ mobile/src/main/java/au/com/lukesleeman/trainwatch/TrainWatchMessageListenerService.java
@@ -39,8 +39,9 @@ public class TrainWatchMessageListenerService extends WearableListenerService {
 
                 // Use that to try and get the nearest stations
 
-                List<Train> trains = PTVWebservice.nextTrains(-37.817119, 144.969458);
-//                List<Train> trains = PTVWebservice.nextTrains(location.getLatitude(), location.getLongitude());
+                // flinder st
+//                List<Train> trains = PTVWebservice.nextTrains(-37.817119, 144.969458);
+                List<Train> trains = PTVWebservice.nextTrains(location.getLatitude(), location.getLongitude());
 
 
                 // Send a message back ...
